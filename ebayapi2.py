e7f682a015020711652946f44997a5d6c6207fcd
DrPi101 <ken-worthington@protonmail.com>
2020-02-24 15:14:17 +0000

    Added new eBay API using txt file with search list

---
+++ ebayapi2.py
@@ -0,0 +1,76 @@
+import json
+from ebaysdk.finding import Connection as finding # install with pip
+from bs4 import BeautifulSoup # install with pip
+from ebayapi import ebayapi # My API key
+
+f = open ("ebay_search.txt", "r")
+lines = f.readlines()
+#print(lines)
+Keywords = lines
+print(Keywords)
+#exit()
+#Keywords = input('Enter your keyword/s eg: white piano:''\n')
+#Keywords = ["Autocourse 1971","Autocourse 1968", "Autocourse 1969"]
+
+def get_items(Keywords):
+	api = finding(appid = ebayapi, siteid='EBAY-GB', config_file=None) # change country with 'siteid='
+	api_request = { 'keywords': Keywords, 'outputSelector' : 'SellerInfo'}
+	response = api.execute('findItemsByKeywords', api_request)
+	soup = BeautifulSoup(response.content, 'lxml')
+	totalentries = int(soup.find('totalentries').text)
+	items = soup.find_all('item')
+	return items
+
+def res_print(items):
+    for item in items:
+        cat = item.categoryname.string.lower()
+        title = item.title.string.lower().strip()
+        price = int(round(float(item.currentprice.string)))
+        url = item.viewitemurl.string.lower()
+        seller = item.sellerusername.text.lower()
+        listingtype = item.listingtype.string.lower()
+
+        print(cat)
+        print("\n")
+        print(title)
+        print()
+        print(price)
+        print()
+        print(url)
+        print()
+        print(seller)
+        print("*" * 20)
+
+
+ebay_results = 'ebay_results.txt' # Next : Write it to JSON
+
+def res_text(items):
+    with open(ebay_results, "a") as f:
+        for item in items:
+            cat = item.categoryname.string.lower()
+            title = item.title.string.lower().strip()
+            price = int(round(float(item.currentprice.string)))
+            url = item.viewitemurl.string.lower()
+            seller = item.sellerusername.text.lower()
+            title = item.title.string.lower().strip()
+            #condition = item.conditiondisplayname.string.lower()
+            f.write(title)
+            f.write(" £")
+            f.write(str(price))
+            f.write(url)
+            f.write(" ")
+            f.write(seller)
+            f.write(" ")
+            #f.write(condition)
+            f.write("*" * 20)
+            f.write("\n")
+
+
+def res_json():
+    pass
+
+# main #
+for i in range(len(Keywords)):
+	x = get_items(Keywords[i])
+	res_print(x)
+	res_text(x)
